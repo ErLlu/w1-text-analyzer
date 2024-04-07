f595ed846f6d1ef5ac9188325fdff7afdd4313ed
Eric Lluis <ericlluis84@gmail.com>
2024-04-07 13:58:24 +0200

    Modify countParagraphs

--- src/analitycs/__tests__/countParagraphs.ts
+++ src/analitycs/__tests__/countParagraphs.ts
@@ -1,18 +1,18 @@
 import { countParagraphs } from "../index.js";
 
 const givenResult = countParagraphs("");
-const expectedResult = 0;
+const result = 0;
 
-const testPassed = expectedResult === givenResult;
+const testPassed = result === givenResult;
 
 if (testPassed) {
   console.log(`text is empty equal 0: ${testPassed}`);
 }
 
 const givenResult2 = countParagraphs("Hola");
-const expectedResult2 = 1;
+const result2 = 1;
 
-const testPassed2 = expectedResult2 === givenResult2;
+const testPassed2 = result2 === givenResult2;
 
 if (testPassed) {
   console.log(testPassed2);
