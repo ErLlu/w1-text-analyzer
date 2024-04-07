5bf20fdaefeab696dd79bbda96f869ec1b71b754
Eric Lluis <ericlluis84@gmail.com>
2024-04-07 22:42:39 +0200

    Add functions countShortWords and countLongWords in index.ts file

--- src/analitycs/index.ts
+++ src/analitycs/index.ts
@@ -1,46 +1,67 @@
 export const countParagraphs = (text: string): number => {
-  const isEmptyText = (text: string): boolean => {
+  const emptyText = (text: string): boolean => {
     return text === "";
   };
-  if (isEmptyText(text)) {
+  if (emptyText(text)) {
     return 0;
   }
-  const paragraphs = text.split("\n");
+  const paragraphs = text.split("\n\n");
 
   return paragraphs.length;
 };
 
 export const countWords = (text: string): number => {
-  const isEmptyText = (text: string): boolean => {
+  const emptyText = (text: string): boolean => {
     return text === "";
   };
   const words = text.trim().split(/[\s\n]+/);
-  const totalWords = words.filter((word) => !isEmptyText(word));
+  const totalWords = words.filter((word) => !emptyText(word));
   return totalWords.length;
 };
 
 export const countCharacters = (text: string): number => {
-  const isEmptyText = (text: string): boolean => {
+  const emptyText = (text: string): boolean => {
     return text === "";
   };
-  if (isEmptyText(text)) {
+  if (emptyText(text)) {
     return 0;
   }
   const characters = text.concat();
   return characters.length;
 };
-export const countShortWords = (
-  text: string,
-  maxLength: number = 4
-): number => {
-  if (!text) {
-    return 0;
+export const countShortWords = (text: string): string[] => {
+  const maximumCharacters: number = 4;
+  const emptyText = (text: string): boolean => {
+    return text === "";
+  };
+
+  if (emptyText(text)) {
+    return [];
   }
 
-  const words: string[] = text.split(/\s/);
+  const words: string[] = text.split(/[\s\n]/);
   const shortWords: string[] = words.filter(
-    (word) => word.length <= maxLength && word !== ""
+    (word) => word.length <= maximumCharacters && word.trim() !== ""
   );
 
-  return shortWords.length;
+  return shortWords;
 };
+
+export const countLongWords = (text: string): string[] => {
+  const minimumCharacters: number = 8;
+  const emptyText = (text: string): boolean => {
+    return text === "";
+  };
+
+  if (emptyText(text)) {
+    return [];
+  }
+
+  const words: string[] = text.split(/[\s\n]+/);
+  const longWords: string[] = words.filter(
+    (word) => word.length >= minimumCharacters
+  );
+
+  return longWords;
+};
+const minimumCharacters: number = 8;
